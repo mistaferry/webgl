d98dcae55f56fb270f71dcd81feca5a6382c88af
Iryna <huryn.iryna@lll.kpi.ua>
2024-11-22 10:39:23 +0100

    pa2

--- Model.js
+++ Model.js
@@ -57,9 +57,9 @@ function Model(name) {
 }
 
 function CreateSurfaceData(data) {
-    let verticesForTriangles = [];
+    let renderVertices = [];
     let allTriangles = [];
-    let allNormals = [];
+    let facetNormals = [];
 
     let uPolysNum = document.getElementById("u").value;
     let vPolysNum = document.getElementById("v").value;
@@ -68,18 +68,18 @@ function CreateSurfaceData(data) {
     // Surface vertex data generation
     let originalVertices = CreateVertexData(uPolysNum, vPolysNum, m, b, n, q, a);
     
-    processData(vPolysNum, uPolysNum, allTriangles, originalVertices, verticesForTriangles, allNormals);
+    processData(vPolysNum, uPolysNum, allTriangles, originalVertices, renderVertices, facetNormals);
 
-    prepareRenderData(data, verticesForTriangles, allTriangles, allNormals);
+    prepareRenderData(data, renderVertices, allTriangles, facetNormals);
 }
 
-function prepareRenderData(data, verticesForTriangles, allTriangles, allNormals){
+function prepareRenderData(data, renderVertices, allTriangles, facetNormals){
     // Populate vertex buffer
-    data.verticesF32 = new Float32Array(verticesForTriangles.length * 3);
-    for (let i = 0, len = verticesForTriangles.length; i < len; i++) {
-        data.verticesF32[i * 3 + 0] = verticesForTriangles[i].p[0];
-        data.verticesF32[i * 3 + 1] = verticesForTriangles[i].p[1];
-        data.verticesF32[i * 3 + 2] = verticesForTriangles[i].p[2];
+    data.verticesF32 = new Float32Array(renderVertices.length * 3);
+    for (let i = 0, len = renderVertices.length; i < len; i++) {
+        data.verticesF32[i * 3 + 0] = renderVertices[i].p[0];
+        data.verticesF32[i * 3 + 1] = renderVertices[i].p[1];
+        data.verticesF32[i * 3 + 2] = renderVertices[i].p[2];
     }
 
     //Populate index buffer
@@ -91,15 +91,15 @@ function prepareRenderData(data, verticesForTriangles, allTriangles, allNormals)
     }
 
     // Populate normal buffer
-    data.normalsF32 = new Float32Array(allNormals.length * 3);
-    for (let i = 0, len = allNormals.length; i < len; i++) {
-        data.normalsF32[i * 3 + 0] = allNormals[i][0];
-        data.normalsF32[i * 3 + 1] = allNormals[i][1];
-        data.normalsF32[i * 3 + 2] = allNormals[i][2];
+    data.normalsF32 = new Float32Array(facetNormals.length * 3);
+    for (let i = 0, len = facetNormals.length; i < len; i++) {
+        data.normalsF32[i * 3 + 0] = facetNormals[i][0];
+        data.normalsF32[i * 3 + 1] = facetNormals[i][1];
+        data.normalsF32[i * 3 + 2] = facetNormals[i][2];
     }
 }
 
-function processData(vPolysNum, uPolysNum, triangles, originalVertices, verticesForTriangles, allNormals) {
+function processData(vPolysNum, uPolysNum, triangles, originalVertices, renderVertices, facetNormals) {
     for (let lineIndex = 1; lineIndex < vPolysNum; lineIndex++) {
         let currentLineOffset = lineIndex * uPolysNum;
         let previousLineOffset = (lineIndex - 1) * uPolysNum;
@@ -111,27 +111,28 @@ function processData(vPolysNum, uPolysNum, triangles, originalVertices, vertices
             let v2ind = currentLineOffset + ((i + 1) % uPolysNum);
 
             // The first triangle
-            createTriangle(v0ind, v1ind, v3ind, originalVertices, verticesForTriangles, triangles, allNormals);
+            createTriangle(v0ind, v1ind, v3ind, originalVertices, renderVertices, triangles, facetNormals);
 
             // The second triangle
-            createTriangle(v2ind, v1ind, v0ind, originalVertices, verticesForTriangles, triangles, allNormals);
+            createTriangle(v2ind, v1ind, v0ind, originalVertices, renderVertices, triangles, facetNormals);
         }
     }
+    // calculateAverageFacetNormal(originalVertices, allTriangles, averageNormals, facetNormals, renderVertices);
 }
 
-function createTriangle(v0ind, v1ind, v2ind, originalVertices, verticesForTriangles, triangles, allNormals){
+function createTriangle(v0ind, v1ind, v2ind, originalVertices, renderVertices, triangles, facetNormals){
     let v0 = originalVertices[v0ind];
     let v1 = originalVertices[v1ind];
     let v2 = originalVertices[v2ind];
 
     // Adding a normal
     let normal = calculateNormal(v0, v1, v2);
-    allNormals.push(normal, normal, normal);
+    facetNormals.push(normal, normal, normal);
 
     // Adding a triangle
-    let trIndex0 = addTriangleVertex(v0, normal, verticesForTriangles);
-    let trIndex1 = addTriangleVertex(v1, normal, verticesForTriangles);
-    let trIndex2 = addTriangleVertex(v2, normal, verticesForTriangles);
+    let trIndex0 = addTriangleVertex(v0, normal, renderVertices);
+    let trIndex1 = addTriangleVertex(v1, normal, renderVertices);
+    let trIndex2 = addTriangleVertex(v2, normal, renderVertices);
 
     let triangle = new Triangle(trIndex0, trIndex1, trIndex2);
     triangles.push(triangle);
@@ -146,11 +147,11 @@ function addTriangleToVertices(triangle, normal, vertices) {
     vertices.forEach(vertex => vertex.normal.push(normal));
 }
 
-function addTriangleVertex(vertex, normal, verticesForTriangles) {
+function addTriangleVertex(vertex, normal, renderVertices) {
     let newVertex = new Vertex([...vertex.p]);
     newVertex.normal = normal;
-    verticesForTriangles.push(newVertex);
-    return verticesForTriangles.length - 1;
+    renderVertices.push(newVertex);
+    return renderVertices.length - 1;
 }
 
 function calculateNormal(v0, v1, v2) {
@@ -177,4 +178,30 @@ function CreateVertexData(uPolysNum, vPolysNum, m, b, n, q, a) {
     }
 
     return allPolylines;
+}
+
+function calculateAverageFacetNormal(originalVertices, allTriangles, averageNormals, facetNormals, renderVertices){
+    for (let i = 300; i < originalVertices.length; i++) {
+        let currV = originalVertices[i];
+        let vTriangles = currV.triangles;
+        let averageNormal = new Vertex([0, 0, 0]);
+
+        // Sum all triangle normals
+        for (let j = 0; j < vTriangles.length; j++) {
+            let normal = facetNormals[vTriangles[j]];
+
+            averageNormal.p[0] += normal[0];
+            averageNormal.p[1] += normal[1];
+            averageNormal.p[2] += normal[2];
+        }
+    
+        // Divide by the number of triangles to get the average
+        averageNormal.p[0] /= vTriangles.length;
+        averageNormal.p[1] /= vTriangles.length;
+        averageNormal.p[2] /= vTriangles.length;
+
+        averageNormal = m4.normalize(averageNormal);
+        averageNormals.push(averageNormal, averageNormal, averageNormal)
+
+    }
 }
